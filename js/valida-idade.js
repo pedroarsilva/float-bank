dfeabf475bdbec013a93cd1f153714a1c681e263
pedroarsilva <pedroarsilva.ti@gmail.com>
2023-07-23 20:57:16 -0300

    JS

---
+++ js/valida-idade.js
@@ -0,0 +1,18 @@
+export default function ehMaiorDeIdade(campo) {
+  const dataNascimento = new Date(campo.value);
+  // console.log(dataNascimento);
+  // validaIdade(dataNascimento);
+
+  // console.log(validaIdade(dataNascimento));
+  if (!validaIdade(dataNascimento)) {
+    campo.setCustomValidity('O usuário não é maior de idade');
+  }
+}
+
+function validaIdade(data) {
+  const dataAtual = new Date();
+  const dataMais18 = new Date(data.getUTCFullYear() +18, data.getUTCMonth(), data.getUTCDate());
+
+
+  return dataAtual >= dataMais18;
+}
